b4b6393dd92e35fedeb67e81656500fa8c652ccf
kabbo25 <43090143+kabbo25@users.noreply.github.com>
2021-02-28 21:15:52 +0600

    maximum_power add kore dilam

---
+++ Maximum_power/test.cpp
@@ -0,0 +1,85 @@
+/*
+* @Author: kabbo
+* @Date:   2020-06-24 08:40:07
+* @Last Modified by:   kabbo
+* @Last Modified time: 2020-06-24 08:49:58
+*/
+#include <bits/stdc++.h>
+using namespace std;
+#define pii pair<long long, long long>
+#define endl '\n'
+#define ull unsigned long long
+#define ll int64_t
+#define ar array
+// http://www.open-std.org/jtc1/sc22/wg21/docs/papers/2016/p0200r0.html
+template <class Fun>
+class y_combinator_result
+{
+    Fun fun_;
+
+public:
+    template <class T>
+    explicit y_combinator_result(T &&fun) : fun_(std::forward<T>(fun)) {}
+
+    template <class... Args>
+    decltype(auto) operator()(Args &&...args)
+    {
+        return fun_(std::ref(*this), std::forward<Args>(args)...);
+    }
+};
+
+template <class Fun>
+decltype(auto) y_combinator(Fun &&fun)
+{
+    return y_combinator_result<std::decay_t<Fun>>(std::forward<Fun>(fun));
+}
+const int mod = 1e9 + 7;
+using u64 = uint64_t;
+using u128 = __uint128_t;
+#define sc1(x) scanf("%lld", &(x));
+mt19937 rnd(chrono::steady_clock::now().time_since_epoch().count());
+/*Well, probably you won't understand anything,
+because you didn't try to understand anything in your life,
+you expect all hard work to be done for you by someone else. 
+Let's start*/
+void solve()
+{
+    int n;
+    cin >> n;
+    vector<ll> v(n + 1, 0);
+    for (int i(1); i <= n; ++i)
+        cin >> v.at(i);
+    ll dp[n + 1][2];
+    memset(dp, 0, sizeof(dp));
+    for (int i(1); i <= n; ++i)
+    {
+        if (v[i] >= 0)
+        {
+            dp[i][0] = max(v[i], dp[i - 1][0] * v[i]);
+            dp[i][1] = v[i] * dp[i - 1][1];
+        }
+        else
+        {
+            dp[i][0] = v[i] * dp[i - 1][1];
+            dp[i][1] = min(v[i], v[i] * dp[i - 1][0]);
+        }
+    }
+    ll mx = (ll)-1e14;
+    for (int i(1); i <= n; ++i)
+        mx = max(mx, dp[i][0]);
+    cout << mx << endl;
+}
+int main()
+{
+
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    // int t;
+    // cin >> t;
+    // for (int i(1); i <= t; ++i)
+    // {
+    //     printf("Case %d:\n", i);
+    solve();
+    //}
+    return 0;
+}
